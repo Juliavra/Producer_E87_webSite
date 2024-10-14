8be0da0aa337e847ecf643dccb6f2cb01ebf2bb0
Julian Avramides <julianavramides@hotmail.com>
2024-10-14 16:19:16 -0300

    TONEJS TEST 2DO

---
+++ js/ToneTest2do.js
@@ -0,0 +1,40 @@
+// for legacy browsers
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+
+// get the audio element
+const audioElement = document.querySelector("audio");
+// pass it into the audio context
+const track = audioContext.createMediaElementSource(audioElement);
+
+track.connect(audioContext.destination);
+
+
+// Select our play button
+const playButton = document.querySelector("button");
+playButton.addEventListener(
+"click",
+() => {
+// Check if context is in suspended state (autoplay policy)
+if (audioContext.state === "suspended") {
+audioContext.resume();
+}
+// Play or pause track depending on state
+if (playButton.dataset.playing === "false") {
+audioElement.play();
+playButton.dataset.playing = "true";
+} else if (playButton.dataset.playing === "true") {
+audioElement.pause();
+playButton.dataset.playing = "false";
+}
+},
+false
+);
+
+audioElement.addEventListener(
+  "ended",
+  () => {
+  playButton.dataset.playing = "false";
+  },
+  false
+  );
