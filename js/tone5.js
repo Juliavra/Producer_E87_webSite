183ffb337d65525d0aa070dd5e03e40781a31b8f
Julian Avramides <julianavramides@hotmail.com>
2024-10-18 02:02:10 -0300

    TONe2html y tone5js actualizados

--- js/tone5.js
+++ js/tone5.js
@@ -1,12 +1,14 @@
 ﻿
 function startsSong() {
+//CREAR AUDIO CONTEXT
+const now = Tone.now();
 /*
 <script src="https://unpkg.com/tone"></script>
 <button id="play-button">Play/Pause</button>
 <script src="music.js"></script>
 */
 //----------xxxx---------------
-
+/*
 const synth = new Tone.Synth().toMaster()
 synth.triggerAttackRelease('C4', '8n')
 
@@ -17,17 +19,22 @@ document.getElementById("playButton").addEventListener("click", function() {
     Tone.Transport.stop();
   }
 });
+*/
+/*
+//--------------------
+// Crea control volumen para el osc1 
+const volumeControl1 = document.querySelector("#volume1");
+volumeControl1.addEventListener(
+  "input",
+  () => {
+   synth.volume = volumeControl1.value;
+  },
+  false
+);
+//-----------------
+*/
 
-
-
-
-const noiseSynth = new Tone.NoiseSynth().toDestination();
-noiseSynth.triggerAttackRelease("2n", 5);
-const seqNoiseSynth  = new Tone.Sequence((time, note) => {
-	noiseSynth.triggerAttackRelease(note, 20, time);
-}).start(0);
-
-
+/*
 //const player = new Tone.Player("https://tonejs.github.io/audio/drum-samples/conga-rhythm.mp3");
 const player = new Tone.Player("https://github.com/Juliavra/Producer_E87_webSite/audio/01%20Dark%20Ringy%20Short%20Loop.mp3");
 player.autostart = true;
@@ -35,45 +42,99 @@ const pitchShift = new Tone.PitchShift(14).toDestination();
 const filter = new Tone.Filter("C0").toDestination();
 // connect a node to the pitch shift and filter in parallel
 player.fan(pitchShift, filter);
-
-
-
+*/
 
 const synth1 = new Tone.Synth({
   oscillator : {
-    volume: 4,
+    volume: -9,
     count: 3,
     spread: 90,
-    type : "sine"
+    type : "triangle"
   }
 }).toDestination();
 
+/*
+synth1.triggerAttack("C0", now);
+synth1.triggerRelease(now + 0.2);
+synth1.triggerAttack("C0", now+0.3);
+synth1.triggerRelease(now + 0.5);
+synth1.triggerAttack("C0", now+0.6);
+synth1.triggerRelease(now + 0.8);
+synth1.triggerAttack("C0", now+0.9);
+synth1.triggerRelease(now + 1.2)
+synth1.triggerAttack("C0", now + 1.2);
+synth1.triggerRelease(now + 1.4);
+synth1.triggerAttack("C0", now + 1.5);
+synth1.triggerRelease(now + 1.7);
+synth1.triggerAttack("C0", now + 1.8);
+synth1.triggerRelease(now + 2);
+synth1.triggerAttack("C0", now + 2.1);
+synth1.triggerRelease(now + 2.3);
+synth1.triggerAttack("C0", now + 2.4);
+synth1.triggerRelease(now + 2.6);
+synth1.triggerAttack("C0", now + 2.7);
+synth1.triggerRelease(now + 2.9);
+
+synth1.triggerAttack("C9", now + 3);
+synth1.triggerRelease(now + 3.2);
+
+synth1.triggerAttack("C0",now + 4 );
+synth1.triggerRelease(now + 4 + 0.2);
+synth1.triggerAttack("C0",now + 4 +0.3);
+synth1.triggerRelease(now + 4 + 0.5);
+synth1.triggerAttack("C0",now + 4 +0.6);
+synth1.triggerRelease(now + 4 + 0.8);
+synth1.triggerAttack("C0",now + 4 +0.9);
+synth1.triggerRelease(now + 4 + 1.2)
+synth1.triggerAttack("C0",now + 4  + 1.2);
+synth1.triggerRelease(now + 4 + 1.4);
+synth1.triggerAttack("C0",now + 4  + 1.5);
+synth1.triggerRelease(now + 4 + 1.7);
+synth1.triggerAttack("C0",now + 4  + 1.8);
+synth1.triggerRelease(now + 4 + 2);
+synth1.triggerAttack("C0",now + 4  + 2.1);
+synth1.triggerRelease(now + 4 + 2.3);
+synth1.triggerAttack("C0",now + 4  + 2.4);
+synth1.triggerRelease(now + 4 + 2.6);
+synth1.triggerAttack("C0",now + 4  + 2.7);
+synth1.triggerRelease(now + 4 + 2.9);
+*/
 
 const seq = new Tone.Sequence((time, note) => {
-	synth1.triggerAttackRelease(note, 2, time);
+//	synth1.triggerAttackRelease(note, 2, time);
 	// subdivisions are given as subarrays
-}, ["C4", "E4", "D4", "E4", "G5",["G6", "A6"],"E2","E2","E2","E1","E1","E0","E2","G2","A2"]).start(0);
+}, ["C4", ["E4", "D4"], "E4", "G5",["G6", "A6"],"E2","E2","E2","E1","E1","E0","E2","G2","A2"]).start(0);
 //}, ["C4", "E4", "C4", "D4",[["G4"], "G4"], "A4", "G4"]).start(0);
 
-
+//--------------------
+// Crea control volumen para el synth1 
+const volumeControl2 = document.querySelector("#volume2");
+volumeControl2.addEventListener(
+  "input",
+  () => {
+ //   synth1.volume = volumeControl2.value;
+   console.log("volumeControl2.value:" + volumeControl2.value);
+  },
+  false
+);
+//-----------------
 const synth2 = new Tone.Synth({
   oscillator : {
-    volume: 4,
+    volume: -24,
     count: 3,
     spread: 70,
     type : "fatsawtooth"
   }
 }).toDestination();
 
+
 const seq2 = new Tone.Sequence((time, note) => {
-	synth2.triggerAttackRelease(note, "8n", time);
-}, ["C4", "E4", "D4", "E4", "G5",["G6", "A6"],"E2","E2","E2","E1","E1","E0","E2","G2","A2"]).start(0);
+//	synth2.triggerAttackRelease(note, "8n", time);
+ }, ["C4", "E4", "D4", "E4", "G5",["G6", "A6"],"E2",[[["E2"]]],"E2","E1","E1","E0","E2","G2","A2"]).start(0);
 
-
-/*
 const synth3 = new Tone.Synth({
   oscillator : {
-    volume: 2,
+    volume: -15,
     count: 3,
     spread: 100,
     type : "sawtooth"
@@ -81,12 +142,12 @@ const synth3 = new Tone.Synth({
 }).toDestination();
 
 const seq3 = new Tone.Sequence((time, note) => {
-	synth3.triggerAttackRelease(note, "32n", time);
+//	synth3.triggerAttackRelease(note, "32n", time);
 }, ["C4", "E4", "D4", "E4", "G5",["G6", "A6"],"E2","E2","E2","E1","E1","E0","E2","G2","A2"]).start(0);
 
 const synth4 = new Tone.Synth({
   oscillator : {
-    volume: 5,
+    volume: -12,
     count: 3,
     spread: 50,
     type : "sine"
@@ -94,14 +155,22 @@ const synth4 = new Tone.Synth({
 }).toDestination();
 
 const seq4 = new Tone.Sequence((time, note) => {
-	synth4.triggerAttackRelease(note, "2n", time);
-}, ["C4", "E4", "D4", "E4", "G5",["G6", "A6"],"E2","E2","E2","E1","E1","E0","E2","G2","A2"]).start(0);
-
+//	synth4.triggerAttackRelease(note, '2n', time);
+//}, ["C4", "E4", "D4", "E4", "G5",["G6", "A6"],"E2","E2","E2","E1","E1","E0","E2","G2","A2"]).start(0);
+//}, ["C4", "E4", "D4", "E4", "G5",["G6", "A6"],]).start(0);
+//}, ["C0", "C0", "A1", "C0"]).start(0);
+//}, ["E1", "C0", "A1", "C0"]).start(0);
+//}, ["E1", "E1", "A1", "C0"]).start(0)
+}, ["E1", "E1", "A1", "C0","D1", "C0", "A1", "C0",
+  "C0", "D0", "A1", "C0","G1", "E0", "A1", "C0"]).start(0)
+/*
 */
 
 
 Tone.Transport.start();
 Tone.Transport.bpm.value = 120;
+
+
 
 
 
@@ -148,9 +217,6 @@ const constructMajorChord = (scale, octave, rootNote) => {
   return chord
 }
 
-
-
-Tone.Transport.bpm.value = 150
 
 
 const IChord = constructMajorChord(AMinorScale, 4, 'A3');
@@ -396,5 +462,28 @@ const synth = new Tone.PolySynth(5, Tone.Synth, {
 }).toMaster();
 */
 
+/*
+//SAMPLER
+const player = new Tone.Player(
+	"https://tonejs.github.io/audio/berklee/gong_1.mp3"
+).toDestination();
+Tone.loaded().then(() => {
+	player.start();
+});
+
+*/
+
+
+
+}
+
+//-*stopsSong----------------
+
+stopsSong
+function stopsSong() {
+  synth1.stop();
+  synth2.stop();
+  synth3.stop();
+  synth4.stop();
 
 }
