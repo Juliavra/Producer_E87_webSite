2c9d8e6464dfc476b88f55df848305a8b4e557e4
Julian Avramides <julianavramides@hotmail.com>
2024-10-20 22:02:15 -0300

    ToneNovember final 20 10 24

--- js/tone7.js
+++ js/tone7.js
@@ -16,7 +16,7 @@ player_1.play();
 const distortion = new Tone.Distortion(0.7).toDestination();
 
 const snareDrum = new Tone.NoiseSynth({
-  volume: -12,
+  volume: -18,
   noise: {
     type: 'white',
     playbackRate: 3,
@@ -43,35 +43,46 @@ const snares = [
   { time: '0:5' },
   { time: '0:6' },
   { time: '0:7' },
-  { time: '0:8' },
   { time: '0:9' },
-  { time: '0:10' },
   { time: '0:11' },
-  { time: '0:12' },
-
+  { time: '0:13' },
 ]
-/* */
 const snarePart = new Tone.Part(function(time) {
   snareDrum.triggerAttackRelease('32n', time)
-}, snares).start(0).stop("+14");
+}, snares).start(0).stop("+13");
 
 
 const bdDrum = new Tone.NoiseSynth({
-  volume: -12,
+  volume: -18,
   noise: {
-    type: 'brown',
+    type: 'pink',
     playbackRate: 33,
   },
   envelope: {
-    attack: 0.0,
-    decay: 0.95,
+    attack: 0.3,
+    decay: 0.25,
     sustain: 0.2,
     release: 0.1,
   },
 }).connect(distortion);
 
+const snares2 = [
+  
+  { time: '0:1' },
+  { time: '0:2' },
+  { time: '0:3' },
+  { time: '0:4' },
+  { time: '0:5' },
+  { time: '0:6' },
+  { time: '0:7' },
+  { time: '0:8' },
+  { time: '0:10' },
+  { time: '0:12' },
+  { time: '0:14' },
+]
+
 const bdPart = new Tone.Part(function(time) {
-  bdDrum.triggerAttackRelease('32n', time)
-}, snares).start(0).stop("+14");
+  bdDrum.triggerAttackRelease('8n', time)
+}, snares2).start(0).stop("+14");
 }
 
