2932e53a8772d86756af81fc17ddaba22e63428d
Julian Avramides <julianavramides@hotmail.com>
2024-11-03 09:15:24 -0300

    tone.vol tests

--- js/htmDePruebas.js
+++ js/htmDePruebas.js
@@ -2,21 +2,22 @@
   setInterval(() => {
     console.log(Tone.immediate());
   }, 1000);
-  /*
-    const feedbackDelay = new Tone.FeedbackDelay("8n", 1).toDestination();
-    feedbackDelay.volume = -9;
-    const pingPong = new Tone.PingPongDelay("4n", 0.2).toDestination();
-    const pingPong_bass = new Tone.PingPongDelay("8n", 0.6).toDestination();
-  */
+
+  var volumen_directo_key = document.getElementById("volume_key");
+  const feedbackDelay = new Tone.FeedbackDelay("2n", 0.6).toDestination();
+  feedbackDelay.volume = -9;
+  /* const pingPong = new Tone.PingPongDelay("4n", 0.2).toDestination();
+   const pingPong_bass = new Tone.PingPongDelay("8n", 0.6).toDestination();
+ */
   // Crear un efecto de reverb
   const reverb = new Tone.Reverb({
-    decay: 4,
+    decay: 0.4,
     wet: 0.4 // Nivel de mezcla del efecto
   }).toDestination(); // Conectar a la salida de audio
-  reverb.generate();
+  // reverb.generate();
 
   const tomline = [
-    { 'time': '0:0', 'note': 'A3', 'duration': '0:4' },
+    { 'time': '1:0', 'note': 'A3', 'duration': '0:2' },
     /*   { 'time': '0:2', 'note': 'F3', 'duration': '0:2' },
       { 'time': '1:2', 'note': 'D3', 'duration': '1:1' },
       { 'time': '2:1', 'note': 'D4', 'duration': '0:0:1' },
@@ -24,31 +25,51 @@
       { 'time': '2:3', 'note': 'F3', 'duration': '1:0:0' },
   */
   ];
-  const tom_vol=new Tone.Volume(-6).toDestination();
-  tom_vol.volume.value = -30;
-  tom_vol.mute = true;
+  const tom_vol = new Tone.Volume(-12).toDestination();
+  const tom_vol_fxSend = new Tone.Volume(-18).connect(feedbackDelay);
+  //tom_vol.volume.value = -30;
+  //tom_vol.mute = true;
 
   const tom = new Tone.MembraneSynth({
     octaves: 2,
     pitchDecay: 7.1,
-    volume: -12
-  }).connect(reverb,tom_vol);
+    volume: -9
+  }).connect(tom_vol);
 
   const tomPart = new Tone.Part(function (time, note) {
     tom.triggerAttackRelease(note.duration, time);
+    console.log("sd original:  ");
   }, tomline).start(0);
 
-  tom.fan(filter);
+  tom.fan(tom_vol_fxSend, tom_vol, reverb);
+
+  const keyline = [
+    { 'time': '0:0', 'note': 'D3', 'duration': '14:0' },
+
+  ];
+
+  const key1 = new Tone.Synth({
+    oscillator: {
+      volume: -21,
+      count: 3,
+      spread: 40,
+      type: "sine"
+    }
+  }).toDestination();
+
+  const keyPart = new Tone.Part(function (time, note) {
+    key1.triggerAttackRelease(note.note, note.duration, time);
+  }, keyline).start(0);
+
+  const key1_vol = new Tone.Volume(-12).toDestination();
 
 
+  volumen_directo_key.addEventListener("change", function (e) {
+    key1_vol.volume = e.currentTarget.value;
+    volumen_directo_key.value = (e.currentTarget.value);
+    volume_key_value.innerHTML = volume_key_value.innerHTML = Math.round(`${e.currentTarget.value * 100}`);
 
-/*const vol = new Tone.Volume(-6).toDestination();
-const osc = new Tone.Oscillator().connect(vol).start();
-vol.volume.value = -20;
-// mute the output
-vol.mute = true;
-*/
-
+  });
 
 
   Tone.Transport.start().stop(30);
