e5442caf3d99be7cb319565c5ad35346b9752407
Julian Avramides <julianavramides@hotmail.com>
2024-11-19 00:35:43 -0300

    noads.html con box para iframe FINAL

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
-const { createApp } = Vue
-createApp({
-    data() {
-        return {
-            mensaje: 'Hola'
-        }
+function cargarLink() {
+    var carpeta = document.getElementById("carpeta");
+    var frame = document.getElementById("frame");
+    var carpeta_trim = carpeta.value.trim();
+    var mensaje = "";
+    for (var f = carpeta_trim.indexOf("https", 0); f < carpeta_trim.length; f++) {
+        if (carpeta_trim[f] != `"`)
+            mensaje += carpeta_trim[f];
+        else { break; }
     }
-}).mount('#app')
+    mensaje = mensaje.trim();
+    console.log(mensaje);
+    //frame.src =mensaje;
+    frame.src = `${mensaje}`;
+    console.log(frame.src);
+
+}
+
+/*
+<iframe width="560" height="315" src="https://www.youtube.com/embed/cHR-S4Ni7PE?si=qh5mdG6Dq0bFh2_d"
+title="YouTube video player" frameborder="0"
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+*/
