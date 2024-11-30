4ff4ac63040e86ad14edb8e4dddad061ede7966d
Julian Avramides <julianavramides@hotmail.com>
2024-11-30 01:26:05 -0300

    Audio_Naming_w_underscore

--- js/step_1_simple_player.js
+++ js/step_1_simple_player.js
@@ -1,13 +1,4 @@
-﻿function startsSong() {
-  setInterval(() => {
-    console.log(Tone.immediate());
-  }, 1000);
-  Tone.Transport.start().stop(17);
-  Tone.Transport.bpm.value = 120;
-  //Tone.Transport.bpm.rampTo(140, 3);
-}
-
-function play1() {
+﻿function play1() {
   const player = new Tone.Player("https://tonejs.github.io/audio/berklee/gong_1.mp3").toDestination();
   // play as soon as the buffer is loaded
   player.autostart = true;
